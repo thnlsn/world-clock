0de014f474bd0f2678165df4d5234f171aff4dad
Thomas Nelson <thnlsn@gmail.com>
2020-06-03 22:58:21 -0700

    setting up state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
 import './App.css';
 
 const App = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
   console.log(moment().format('h:mm:ss a'));
   return (
     <div className='App'>
