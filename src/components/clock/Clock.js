d630fd94cdb9b390ffc2df42fa29942ffb2c2df7
Thomas Nelson <thnlsn@gmail.com>
2020-06-07 23:53:29 -0700

    clock appearing correctly

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -5,8 +5,8 @@ const Clock = () => {
   return (
     <Fragment>
       <div className='clock-container'>
-        <div className='clock borderSolid borderWidth-xl borderRadius-50 bg-cover'>
-          <span className='clock__hand-hinge borderRadius-50'></span>
+        <div className='clock'>
+          <span className='clock__hand-hinge'></span>
         </div>
       </div>
     </Fragment>
