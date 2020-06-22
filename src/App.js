82ecf0c333c18130d9666c3f9a52c780e9cfe40a
Thomas Nelson <thnlsn@gmail.com>
2020-06-21 21:42:10 -0700

    timestate updated realtime every second

--- src/App.js
+++ src/App.js
@@ -10,10 +10,25 @@ const App = () => {
   const [minutes, setMinutes] = useState(moment().format('mm'));
   const [seconds, setSeconds] = useState(moment().format('ss'));
   const [period, setPeriod] = useState(moment().format('a'));
+
+  const updateTime = () => {
+    setInterval(() => {
+      console.log('interval called');
+      setHours(moment().format('h'));
+      setMinutes(moment().format('mm'));
+      setSeconds(moment().format('ss'));
+      setPeriod(moment().format('a'));
+    }, 1000);
+  };
+
+  useEffect(() => {
+    updateTime();
+  }, []);
+
   return (
     <div className='App'>
       {hours}:{minutes}:{seconds} {period}
-      <Clock hours={hours} />
+      <Clock hours={hours} minutes={minutes} seconds={seconds} />
     </div>
   );
 };
