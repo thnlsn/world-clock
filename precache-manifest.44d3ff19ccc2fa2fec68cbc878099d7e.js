640bc5c5c2f46841abb924077f86c95a991da912
Thomas Nelson <thnlsn@gmail.com>
2020-06-23 17:33:19 -0700

    Updates

---
+++ precache-manifest.44d3ff19ccc2fa2fec68cbc878099d7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5cda805134c2c611e2d0fc9c3e386c",
+    "url": "/world-clock/index.html"
+  },
+  {
+    "revision": "ba85c356d106f4466c2d",
+    "url": "/world-clock/static/css/main.637436f4.chunk.css"
+  },
+  {
+    "revision": "3b239ba58c8771dafe39",
+    "url": "/world-clock/static/js/2.25fa0292.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/world-clock/static/js/2.25fa0292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba85c356d106f4466c2d",
+    "url": "/world-clock/static/js/main.b854a588.chunk.js"
+  },
+  {
+    "revision": "e4edf54ad1dc3d28fbd6",
+    "url": "/world-clock/static/js/runtime-main.c6f31c24.js"
+  },
+  {
+    "revision": "ec97d5ffd095fe9e8bb518cdfdf29b6e",
+    "url": "/world-clock/static/media/clock.ec97d5ff.png"
+  }
+]);
