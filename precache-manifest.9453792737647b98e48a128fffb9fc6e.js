eeea1eb857630564d6ed454c0959d506c7c5cc0d
Thomas Nelson <thnlsn@gmail.com>
2021-01-20 23:14:52 -0800

    Updates

---
+++ precache-manifest.9453792737647b98e48a128fffb9fc6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de93f267340c3cae252fa4b5b0fdd9a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7adbf820a5025d62b3d1",
+    "url": "/static/css/main.07209f48.chunk.css"
+  },
+  {
+    "revision": "8a4959dea8cddd63c45c",
+    "url": "/static/js/2.aff946b2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.aff946b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7adbf820a5025d62b3d1",
+    "url": "/static/js/main.bd97c68d.chunk.js"
+  },
+  {
+    "revision": "dcab649990da9881bce4",
+    "url": "/static/js/runtime-main.c1328ad8.js"
+  },
+  {
+    "revision": "ec97d5ffd095fe9e8bb518cdfdf29b6e",
+    "url": "/static/media/clock.ec97d5ff.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/world-clock/precache-manifest.adb6772992b76dd2ae04273a11bd1fe7.js"
+  "/precache-manifest.9453792737647b98e48a128fffb9fc6e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/world-clock/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
